318c317b8402d6cebb920e02704e888ad9ee3a2f
Grayblood <luismedinafernandes@gmail.com>
2024-10-21 17:11:06 +0200

    second try

--- lista-invitados/src/app/api.js
+++ lista-invitados/src/app/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const instance = axios.create({ baseURL: 'http://localhost:8080/people' });
+const instance = axios.create({ baseURL: 'https://springreacttest.onrender.com/people' });
 
 export const savePersonName = async (obj) => await instance.post('/', obj)
 
